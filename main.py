3cc13e07c2f831352df0fee3e72db64e4fb4baf9
chenzongwei <17695480342@163.com>
2025-07-21 00:16:44 +0800

    更新uvicorn运行配置，修改端口为8666并启用自动重载功能。

--- main.py
+++ main.py
@@ -229,4 +229,4 @@ async def websocket_endpoint(websocket: WebSocket, room_name: str):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000) 
+    uvicorn.run("main:app", host="0.0.0.0", port=8666, reload=True) 
